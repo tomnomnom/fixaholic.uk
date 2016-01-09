84436f9999da076d1951ab56912bc251e1b93a5a
Tom Hudson <mail@tomnomnom.com>
2016-01-09 05:51:03 -0500

    Adds favicon

--- lib/header.php
+++ lib/header.php
@@ -2,9 +2,11 @@
 <html lang="en">
     <head>
         <title>fixaholic.uk</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato"/>
         <link rel="stylesheet" type="text/css" href="/main.css"/>
+        <link rel="icon" type="image/png" href="/favicon-512.png"/>
+        <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="/favicon-512.png"/>
     </head>
     <body>
         <h1><a href="/">fixaholic.uk</a></h1> 
