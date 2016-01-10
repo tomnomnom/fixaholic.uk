6ec266178694a777fb34a1d7b25bf5da01f94902
Tom Hudson <mail@tomnomnom.com>
2016-01-09 19:29:20 -0500

    Adds logo, text tweaks, meta tags

--- lib/header.php
+++ lib/header.php
@@ -3,10 +3,12 @@
     <head>
         <title>fixaholic.uk</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="theme-color" content="#363840">
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato"/>
         <link rel="stylesheet" type="text/css" href="/main.css"/>
         <link rel="icon" type="image/png" href="/favicon-512.png"/>
         <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="/favicon-512.png"/>
     </head>
     <body>
-        <h1><a href="/">fixaholic.uk</a></h1> 
+        <h1><a href="/"><img src="/logo.png" alt="fixaholic.uk" class="logo"></a></h1> 
